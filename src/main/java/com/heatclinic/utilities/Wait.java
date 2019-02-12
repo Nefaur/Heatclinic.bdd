123c1fdf3c0a7745c7d4ae27a597360ecb6b6cc0
Nefaur <Nefaur.rb@gmail.com>
2019-02-11 21:57:08 -0500

    Added webdriver wait function

--- src/main/java/com/heatclinic/utilities/Wait.java
+++ src/main/java/com/heatclinic/utilities/Wait.java
@@ -1,5 +1,16 @@
 package com.heatclinic.utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Wait {
 
+	public void explicitWait(WebDriver driver, By by) {
+		WebElement myDynamicElement = (new WebDriverWait(driver, 10))
+				  .until(ExpectedConditions.visibilityOfElementLocated(by));
+	}
+	
 }
